93e4d6c3771fbe9d51516478a970707661570037
Kamil <Kamil@KAMIL-PC>
2021-03-31 17:21:12 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,271 @@
+﻿
+namespace KiT_Toolbox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(2, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1457, 686);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button7);
+            this.tabPage1.Controls.Add(this.button6);
+            this.tabPage1.Controls.Add(this.button5);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1449, 660);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Ogólne";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(664, 222);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(201, 47);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "System Activation";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(448, 221);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(201, 48);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Instalacja DirectX Redistributable";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(228, 221);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(201, 48);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Instalacja VC++ Redistributable";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button5.Location = new System.Drawing.Point(6, 221);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(201, 48);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Skanowanie Antywirusowe";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(708, 162);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Opcjonalne:";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button4.Location = new System.Drawing.Point(664, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Fix Error 2503";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(448, 56);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Change the service mode to \"Extreme\"";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(228, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Change the service mode  \"Safe\"";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(6, 57);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Ponownie uruchom usługę drukowania";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(708, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Naprawcze:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.button10);
+            this.tabPage2.Controls.Add(this.button9);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1449, 660);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Dodatki";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(121, 62);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(109, 25);
+            this.button10.TabIndex = 2;
+            this.button10.Text = "GPU-Z";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(6, 62);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(109, 25);
+            this.button9.TabIndex = 1;
+            this.button9.Text = "CPU-Z";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(683, 3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Dodatki :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1456, 688);
+            this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "KiT Toolbox";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Net;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KiT_Toolbox
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Process.Start("sc.exe", "Stop Spooler");
+            Process.Start("sc.exe", "Start Spooler");
+            MessageBox.Show("Gotowe");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //  I have a problem with changing the service start mode
+
+            Process.Start("sc.exe", "Start WdiServiceHost");
+
+            Process.Start("sc.exe", "config [XboxGipSvc] start= demand");
+            Process.Start("sc.exe", "Config [XblAuthManager] state= demand");
+            Process.Start("sc.exe", "Config [XblGameSave] state= demand");
+            Process.Start("sc.exe", "Config [xboxgip] state= demand");
+            Process.Start("sc.exe", "Config [vds] state= demand");
+            Process.Start("sc.exe", "Config [WdiSystemHost] state= demand");
+            Process.Start("sc.exe", "Config [wscsvc] state= auto");
+            Process.Start("Sc.exe", "Start wscsvc");
+            Process.Start("sc.exe", "Config [DPS] state= auto");
+            Process.Start("sc.exe", "Start DPS");
+            Process.Start("sc.exe", "Config [diagsvc] state= demand");
+            Process.Start("sc.exe", "Config [VSS] state= demand");
+            Process.Start("sc.exe", "Config [SDRSVC] state= demand");
+            Process.Start("sc.exe", "Config [swprv] state= demand");
+            Process.Start("sc.exe", "config start= auto BITS");
+            Process.Start("sc.exe", "Start BITS");
+            Process.Start("sc.exe", "Config [PcaSvc] state= demand");
+            Process.Start("sc.exe", "Start PcaSvc");
+            Process.Start("sc.exe", "Config [WerSvc] state= demand");
+            Process.Start("sc.exe", "Config [SecurityHealthService] state= demand");
+            MessageBox.Show("Services set to Safe mode");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  I have a problem with changing the service start mode
+            Process.Start("sc.exe", "Stop WdiServiceHost");
+            Process.Start("sc.exe", "config [WdiServiceHost] state= disabled");
+            Process.Start("sc.exe", "Stop XboxGipSvc");
+            Process.Start("sc.exe", "config [XboxGipSvc] state= disabled");
+            Process.Start("sc.exe", "Stop XblAuthManager");
+            Process.Start("sc.exe", "config [XblAuthManager] state= disabled");
+            Process.Start("sc.exe", "Stop XblGameSave");
+            Process.Start("sc.exe", "config [XblGameSave] state= disabled");
+            Process.Start("sc.exe", "Stop xboxgip");
+            Process.Start("sc.exe", "config [xboxgip] state= disabled");
+            Process.Start("sc.exe", "Stop vds");
+            Process.Start("sc.exe", "config vds state= disabled");
+            Process.Start("sc.exe", "Stop WdiSystemHost");
+            Process.Start("sc.exe", "config [WdiSystemHost] state= disabled");
+            Process.Start("Sc.exe", "Stop wscsvc");
+            Process.Start("sc.exe", "config [wscsvc] state= disabled");
+            Process.Start("sc.exe", "Stop DPS");
+            Process.Start("sc.exe", "config [DPS] state= disabled");
+            Process.Start("sc.exe", "Stop diagsvc");
+            Process.Start("sc.exe", "config [diagsvc] state= disabled");
+            Process.Start("sc.exe", "Stop VSS");
+            Process.Start("sc.exe", "config [VSS] state= disabled");
+            Process.Start("sc.exe", "Stop SDRSVC");
+            Process.Start("sc.exe", "Config [SDRSVC] state= disabled");
+            Process.Start("sc.exe", "Stop swprv");
+            Process.Start("sc.exe", "Config [swprv] state= disabled");
+            Process.Start("sc.exe", "Stop BITS");
+            Process.Start("sc.exe", "Config [BITS] state= disabled");
+            Process.Start("sc.exe", "Stop PcaSvc");
+            Process.Start("sc.exe", "Config [PcaSvc] state= disabled");
+            Process.Start("sc.exe", "Stop WerSvc");
+            Process.Start("sc.exe", "Config [WerSvc] state= disabled");
+            Process.Start("sc.exe", "Stop SecurityHealthService");
+            Process.Start("sc.exe", "Config [SecurityHealthService] state= disabled");
+            MessageBox.Show("Services set to Extreme mode");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // Fixing error %Windir\Temp%  Permisions 
+            // My knowledge of C # is too little to write this
+            MessageBox.Show("Fixed Error 2503 reboot required ");
+            Process.Start("shutdown.exe", "-r -t 10");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string root = @"C:\Temp";
+            if (!Directory.Exists(root))
+
+                Directory.CreateDirectory(root);
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://adwcleaner.pl/wp-content/uploads/program/adwcleaner_8.2.exe", @"c:\Temp\adw.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process p = new Process();
+            p.StartInfo.FileName = "adw.exe";
+            p.StartInfo.Arguments = @"c:\Temp\adw.exe /eula /clean /preinstalled /noreboot";
+            p.Start();
+
+
+
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://devbuilds.s.kaspersky-labs.com/devbuilds/KVRT/latest/full/KVRT.exe", @"c:\Temp\KVRT.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process.Start(@"c:\Temp\KVRT.exe");
+
+            p.WaitForExit();
+            MessageBox.Show("Skanowanie zakończone");
+
+        }
+          
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string root = @"C:\Temp";
+            if (!Directory.Exists(root))
+
+                Directory.CreateDirectory(root);
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://github.com/kamiloxf/w10/releases/download/1/VisualCppRedist_AIO_x86_x64.exe", @"c:\Temp\VisualCppRedist_AIO_x86_x64.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process.Start(@"c:\Temp\VisualCppRedist_AIO_x86_x64.exe");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string root = @"C:\Temp";
+            if (!Directory.Exists(root))
+
+                Directory.CreateDirectory(root);
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://github.com/kamiloxf/w10/releases/download/1/DXrtAiO.exe", @"c:\Temp\dx.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process.Start(@"c:\Temp\dx.exe");
+            MessageBox.Show("DirectX Zainstalowany");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            //Windows Activator
+            // My knowledge of C # is too little to write this
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string root = @"C:\Dodatki";
+            if (!Directory.Exists(root))
+
+                Directory.CreateDirectory(root);
+
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://download.cpuid.com/cpu-z/cpu-z_1.95-en.exe", @"c:\Dodatki\cpu-z.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process c = new Process();
+            c.StartInfo.FileName = "cpu-z.exe";
+            c.StartInfo.Arguments = @"c:\Dodatki\cpu-z.exe /silent";
+            c.Start();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string root = @"C:\Dodatki";
+            if (!Directory.Exists(root))
+                Directory.CreateDirectory(root);
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            using (WebClient webClient = new WebClient())
+                webClient.DownloadFile("https://github.com/kamiloxf/w10/releases/download/1/GPU-Z.exe", @"c:\Dodatki\gpu-z.exe");
+            Directory.SetCurrentDirectory(root);
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            Process g = new Process();
+            g.StartInfo.FileName = "gpu-z.exe";
+            g.StartInfo.Arguments = @"c:\Dodatki\gpu-z.exe /silent";
+            g.Start();
+        }
+    }
+}
